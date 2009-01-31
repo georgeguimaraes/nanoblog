e1d07ec9e23705a8da5cd7a0e97c16f1320e50d5
George Guimaraes <george.guimaraes@gmail.com>
2009-01-31 20:43:40 -0200

    Model User criado

--- db/schema.rb
+++ db/schema.rb
@@ -9,10 +9,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090131204304) do
+ActiveRecord::Schema.define(:version => 20090131223833) do
 
   create_table "posts", :force => true do |t|
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "hashed_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
